5fdeb6037e479040347138dd8a9d3c964287f0c5
joowon <one.joowon@gmail.com>
2020-08-13 17:53:51 +0900

    양방향 이벤트 핸들링

---
+++ src/App.svelte
@@ -0,0 +1,40 @@
+<script>
+	export let name;
+	let color = 'black';
+
+	const handleClick = () => {
+		color = 'blue';
+	};
+
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<h3>hi, my name is one!</h3>
+	<p style='color: {color}'>{color} beammmmmmm!!!!!</p>
+	<button on:click={handleClick}>update jean color</button>
+	<input type='text' bind:value={color} />
+
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
